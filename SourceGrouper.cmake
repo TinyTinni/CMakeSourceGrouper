520121b87e2648ebcfdca1772b4b3cf68e6391a6
Matthias Möller <m_moeller@live.de>
2016-06-09 11:30:18 +0200

    Update SourceGrouper.cmake

--- SourceGrouper.cmake
+++ SourceGrouper.cmake
@@ -2,6 +2,7 @@
 # The MIT License (MIT)
 # 
 # Copyright (c) 2016 Matthias Möller
+# https://github.com/TinyTinni/CMakeSourceGrouper
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
